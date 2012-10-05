298abeda20eccd741d55b2c97a176de0312e2838
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-10-05 22:39:32 +0200

    Restore files from backup

--- src/string/bstring.c
+++ src/string/bstring.c
@@ -40,7 +40,6 @@ INA_API(ina_str_t) ina_str_new(ina_mempool_t *pool)
     return ina_str_newlen("", 0, pool);
 }
 
-<<<<<<< HEAD
 INA_API(ina_rc_t) ina_str_free(ina_str_t s)
 {
     free(s);
@@ -48,15 +47,6 @@ INA_API(ina_rc_t) ina_str_free(ina_str_t s)
 }
 
 INA_API(ina_str_t) ina_str_dup(const ina_str_t s, ina_mempool_t *pool)
-=======
-INA_API(ina_rc_t) ina_str_free(ina_str_t s, ina_mempool_t *pool)
-{
-    free(s);
-    return INA_RC_OK;
-}
-
-INA_API(ina_str_t) ina_str_dup(ina_str_t s, ina_mempool_t *pool)
->>>>>>> 244ecc23bf8bc8d3f4c0def4823befca126b443a
 {
     return ina_str_newlen(s, strlen(s), pool);
 }
@@ -66,22 +56,14 @@ INA_API(const char*) inac_str_cstr(ina_str_t s)
     return strdup(s);
 }
 
-<<<<<<< HEAD
 INA_API(ina_str_t) ina_str_cat(ina_str_t dest, const ina_str_t src)
-=======
-INA_API(ina_str_t) ina_str_cat(const ina_str_t s1, const ina_str_t s2, ina_mempool_t *pool)
->>>>>>> 244ecc23bf8bc8d3f4c0def4823befca126b443a
 {
     return NULL;
 }
 
 INA_API(ina_rc_t) ina_str_cmp(const ina_str_t s1, const ina_str_t s2)
 {
-<<<<<<< HEAD
     return INA_SUCCESS;
-=======
-    return INA_RC_OK;
->>>>>>> 244ecc23bf8bc8d3f4c0def4823befca126b443a
 }
 
 INA_API(const ina_str_t) ina_str_strstr(const ina_str_t s1, const ina_str_t s2)
