af0674f2299e8d5ee7979bd6cf23b38ee4d593ec
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-10-09 01:06:58 +0200

    Macros fixed

--- include/libinac/test.h
+++ include/libinac/test.h
@@ -32,12 +32,14 @@
  
  #ifdef DEBUG
  #define INA_ASSERT(cond) assert(cond)
- #define INA_ASSERT_NULL(v) INA_ASSERT(v != NULL)
- #define INA_ASSERT_EQUAL(expected, actual) INA_ASSERT(expected == acutal)
+ #define INA_ASSERT_NULL(v) INA_ASSERT(v == NULL)
+ #define INA_ASSERT_NOTNULL(v) INA_ASSERT(v != NULL)
+ #define INA_ASSERT_EQUAL(expected, actual) INA_ASSERT(expected == actual)
  #define INA_ASSERT_NOTEQUAL(notexpected, actual) INA_ASSERT(notexpected != actual)
  #else
  #define INA_ASSERT(cond)
  #define INA_ASSERT_NULL(v)
+ #define INA_ASSERT_NOTNULL(v)
  #define INA_ASSERT_EQUAL(expected, actual)
  #define INA_ASSERT_NOEQUAL(notexpected, actual)
  #endif
