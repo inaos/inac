8018900d062ab746bf71fe1d55772eec812936a4
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-10-10 23:32:19 +0200

    indent fixed

--- include/libinac/lib.h
+++ include/libinac/lib.h
@@ -50,8 +50,8 @@
 /* Version as a 3-byte hex number, e.g. 0x010201 == 1.2.1. Use this
  * for numeric comparisons, e.g. #if INA_VERSION_HEX >= ... */
 #define INA_VERSION_HEX  ((INA_MAJOR_VERSION << 16) |   \
-                              (INA_MINOR_VERSION << 8)  |   \
-                              (INA_MICRO_VERSION << 0))
+                          (INA_MINOR_VERSION << 8)  |   \
+                          (INA_MICRO_VERSION << 0))
 
 /*
  * Startup application with argc, argv in order to deal with 
