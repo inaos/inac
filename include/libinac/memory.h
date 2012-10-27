682c60883e78b98e82bd03398b949bb01d70e3d8
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-10-27 09:18:39 +0200

    Typo fixed

--- include/libinac/memory.h
+++ include/libinac/memory.h
@@ -44,7 +44,7 @@
 /* Memory pool handle */
 typedef struct ina_mempool_s  {
     int pid; /* FIXME: x-platform */
-    int tid; /* FIXME: x-plaform */
+    int tid; /* FIXME: x-platform */
     ina_shm_handle_t shm_handle;
     uint32_t cf;
     size_t size;
