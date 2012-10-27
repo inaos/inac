c918ae49bf6e644f73c13fac274be4fd9337ab5d
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-10-27 15:10:38 +0200

    Assert fixed

--- src/error.c
+++ src/error.c
@@ -67,7 +67,7 @@ INA_API(ina_rc_t) ina_err_push(int mod, int fn, int reason, ina_str_t file,
     INA_ASSERT(__initialized);
     INA_ASSERT(mod <= 64);
     INA_ASSERT(fn <= 32);
-    INA_ASSERT(reason <= 512);
+    INA_ASSERT(reason <= 1023);
     INA_ASSERT_NOTNULL(file);
     INA_ASSERT(line > 0);
     INA_ASSERT_NOTNULL(msg);
