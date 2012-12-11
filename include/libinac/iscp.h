dcd938c2e092b1ab94cc6e3fe3a966ec5d7961ae
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-12-11 15:11:39 +0100

    Added header for INAOS Simple Command Protocol

---
+++ include/libinac/iscp.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2012, INAOS GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the INAOS GmbH nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software 
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL INAOS GmbH BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+#ifndef _LIBINAC_ISCP_H_
+#define _LIBINAC_ISCP_H_
+
+#include <libinac/lib.h>
+
+/*
+ * INAOS simple command prootocol 
+ */
+
+#define INA_ISCP_BUFSIZE   (2048)  /* Max size of command data */
+
+#define INA_ISCP_TYPE_INT     (0)  /* uint32_t  4 bytes*/
+#define INA_ISCP_TYPE_STRING  (1)  /* uint16_t (lenght) + char[lenght] */
+#define INA_ISCP_TYPE_FLOAT   (2)  /* float 8 bytes */
+
+
+/* Command handler */
+typedef ina_rc_t *(ina_ispc_cmdhandler)(int cmd_id, int count, ina_iscp_param_t*);
+
+/* ISCP parameter */
+typedef struct ina_ispc_param_s {
+    uint8_t type;
+    union {
+        int32_t   n;
+        float     f;
+        ina_str_t s;
+    } value;
+} ina_ispc_param_t;
+
+/* internal send/receive buffer */
+typedef struct ina_iscp_buf_s {
+    uint16_t length;    /* store the command buffer size */
+    uint32_t cmd_uid;   /* UID for sent commands */
+    uint16_t cmd_id;    /* identify the command */
+    uint16_t pcount;    /* number of parameters */
+    /* Parameters 
+     * [1 byte, parameter type][parameter]
+     * 
+     * [INA_ISCP_TYPE_INT][b1][b2][b3][b4]
+     * [INA_ISPP_TYPE_STRING][string lenght b1][b2 str lenght][b1][b2][bxx...]
+     * [INA_ISCP_TYPE_FLOAT][b1][b2][b3][b4][b5][b6][b7][b8]
+     */
+    unsigned char cmd_data[INA_ISCP_BUFSIZE]; 
+} ina_iscp_buf_t;
+
+/*
+ * Initialize internal structrues for ISCP
+ */
+INA_API(in_rc_t) ina_iscp_init(void);
+/*
+ * Register  command definition. Only used on "server" side.
+ *
+ * Parameters
+ * cmd_id       Command identifier
+ * handler      Command handler function
+ *
+ * Return Value
+ * INA_SUCCESS if no error occurred
+ */
+INA_API(ina_rc_t) ina_iscp_register(int cmd_id, ina_iscp_cmdhandler handler);
+/*
+ * Reset ISCP status an remove all regsitred commands.
+ *
+ * Return Value:
+ * INA_SUCCESS if successfully cleared.
+ */
+INA_API(int_rc_t) ina_iscp_reset(void);
+/*
+ * Send a command synchronously.
+ * Like:
+ * ina_iscp_send(fd, INAFX_ISCP_SUBSCRIBE, 
+ *                   INA_ISCP_TYPE_INT, 1,
+ *                   INA_ISCP_TYPE_STR, "127.0.0.1"
+ *                   INA_ISCP_TYPE_FLOAT, 2.3,
+ *                   INA_ISCP_TYPE_INT, 3);
+ *
+ * Parameters
+ * fd       socket descriptor
+ * cmd_id   Commmand identifier
+ * ...      Command parameter list
+ *
+ * Return Value
+ * INA SUCCESS if command was successfully sent to the server and 
+ *             executed by the receiver w/o error.
+ */
+INA_API(ina_rc_t) ina_iscp_send(int fd, int cmd_id, ...);
+
+/*
+ * Check and receive a previously regsisterd command. If a
+ * command was received the command handler will be invoked
+ * synchronously.
+ *
+ * Parameters
+ * fd       Socket descriptior
+ * nc       Max number of command to accept.
+ * timeout  Number of milliseconds to wait for a command
+ *
+ * Return Value:
+ * INA_SUCCESS if no error occurred.
+ */
+INA_API(ina_rc_t) ina_ispc_recv(int fd, int nc, int timeout);
+
+#endif
+
