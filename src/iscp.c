0d08ff4bc0084d178f3682b96eeaa08240e21994
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-12-15 17:47:29 +0100

    Read full messag at once (non blocking)

--- src/iscp.c
+++ src/iscp.c
@@ -316,14 +316,11 @@ INA_API(ina_rc_t) ina_iscp_net_recv_cb(ina_iscp_ctx_t *ctx, size_t *size,
     fd = *(int*)ctx->data;
  
     nread = ina_net_read(fd, (char*)buf, sizeof(uint16_t));
-    if (nread) {
+    if (nread > 0) {
         int length;
         length = (*(uint16_t*)&buf[0]); 
-        nread = ina_net_read(fd, (char*)buf[sizeof(uint16_t)], length-sizeof(uint16_t));
-        if (nread) {
-            *size = length;
-            return INA_SUCCESS;
-        }
+        *size = length;
+        return INA_SUCCESS;
     }
     return INA_FAILURE;
 }
