f463019c5070e532aea5b500f774bf8c7d98040d
Rocco Siegrist <rocco.siegrist@matselect.ch>
2012-12-24 00:11:32 +0100

    Added unit tests for time module

---
+++ tests/test_time.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2012, INAOS GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the INAOS GmbH nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software 
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL INAOS GmbH BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+#include <libinac/lib.h>
+
+void test_time_read_clock() 
+{
+    struct timeval tv;
+    ina_time_t t;
+    time_t ms;
+    time_t secs;
+    time_t secs2;
+
+    INA_TRACE_MSG("test_time_read_clock");
+
+    ms = 0;
+    secs = 0;
+    secs2 = 0;
+
+    gettimeofday(&tv, NULL);
+    INA_ASSERT_SUCCEED(ina_time_read_clock(&t));
+    INA_ASSERT_SUCCEED(ina_time_get_seconds(&t, &secs));
+    INA_ASSERT_SUCCEED(ina_time_get_seconds(&t, &secs2));
+    INA_ASSERT(secs > 0);
+    INA_ASSERT_EQUAL(secs, secs2);
+    INA_ASSERT_EQUAL(tv.tv_sec, secs);
+    INA_ASSERT_SUCCEED(ina_time_get_milliseconds(&t, &ms));
+    INA_ASSERT(ms > 0);
+    INA_TRACE3("tv.tv_usec=%d", tv.tv_usec);
+    INA_TRACE3("ms=%ld", ms);
+    INA_ASSERT_EQUAL(tv.tv_usec/1000, ms);
+}
+
