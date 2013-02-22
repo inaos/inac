d19d6552b9c9ad7dbbee1d4e80c741f051582304
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-02-22 07:36:43 +0100

    Unit Tests

--- include/libinac/debug.h
+++ include/libinac/debug.h
@@ -82,8 +82,9 @@ extern "C" {
 
 #define INA_TEST_RUN(name, pattern) if (strstr(name, pattern)) #name()
 
-#define INA_TEST_SPAWN_BEGIN()                           \
- {  
+#define INA_TEST_SPAWN_BEGIN()      \
+ {                                  \
+     int _spawn = 0, FILE _fp = 0;
 
 #define INA_TEST_SPAWN_CODE_BEGIN()  \
    if (_spawn) {
