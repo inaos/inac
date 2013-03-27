615e59fece4264d9546066a784215e9f274c9b04
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-03-27 17:59:00 +0100

    Changed time mesurement for timestamp

--- src/time.c
+++ src/time.c
@@ -29,7 +29,7 @@
 #include "config.h"
 
 #ifdef INA_OS_WIN32
-#define __INA_TIME_INC(vv_ptr) InterlockedIncrement64(vv_ptr)
+#define __INA_TIME_INC(vv_ptr) InterlockedExchangeAdd64(vv_ptr, 1)
 #else
 #define __INA_TIME_INC(vv_ptr) __sync_fetch_and_add(vv_ptr, 1) 
 #endif
@@ -112,7 +112,7 @@ INA_API(ina_rc_t) ina_time_stopwatch_create(ina_stopwatch_t **stopwatch, int id,
 
 INA_API(ina_rc_t) ina_time_stopwatch_open(ina_stopwatch_t **stopwatch, int id)
 {
-    return __ina_stopwatch_init(id, stopwatch, 0, 0);
+    return __ina_stopwatch_init(id, stopwatch, 0, INA_TIME_MAX_STAMPS);
 }
 
 INA_API(ina_rc_t) ina_time_stopwatch_started(ina_stopwatch_t *stopwatch)
@@ -203,11 +203,22 @@ INA_API(ina_rc_t) ina_time_stopwatch_read_stamp(ina_stopwatch_t* stopwatch, int6
     if (stopwatch->ts->sec_duration == 0) {
         #ifdef INA_OS_WIN32
         LARGE_INTEGER elapsed;
-        elapsed.QuadPart = stopwatch->ts->stamp.tp.QuadPart - stopwatch->tv->start.tp.QuadPart; 
+        if (*stamp_index == 0) {
+            elapsed.QuadPart = stopwatch->ts->stamp.tp.QuadPart - stopwatch->tv->start.tp.QuadPart; 
+        } else {
+           ina_stopwatch_ts_t *ts =  (&(stopwatch->tv->stamps))+(*stamp_index-1);
+           elapsed.QuadPart = stopwatch->ts->stamp.tp.QuadPart - ts->stamp.tp.QuadPart; 
+        }
         stopwatch->ts->sec_duration = __ina_lit_to_secs(&elapsed);
         #else
-        stopwatch->ts->sec_duration = (stopwatch->ts->stamp.tp.tv_sec - stopwatch->tv->start.tp.tv_sec);
-        stopwatch->ts->sec_duration += ((stopwatch->ts->stamp.tp.tv_usec - stopwatch->tv->start.tp.tv_usec) / 10000000.0); 
+        if (*stamp_index == 0) {
+            stopwatch->ts->sec_duration = (stopwatch->ts->stamp.tp.tv_sec - stopwatch->tv->start.tp.tv_sec);
+            stopwatch->ts->sec_duration += ((stopwatch->ts->stamp.tp.tv_usec - stopwatch->tv->start.tp.tv_usec) / 10000000.0);
+        } else {
+            ina_stopwatch_ts_t *ts =  (&(stopwatch->tv->stamps))+(*stamp_index-1);
+            stopwatch->ts->sec_duration = (stopwatch->ts->stamp.tp.tv_sec - ts->stamp.tp.tv_sec);
+            stopwatch->ts->sec_duration += ((stopwatch->ts->stamp.tp.tv_usec - ts->stamp.tp.tv_usec) / 10000000.0);         
+        } 
         #endif
         stopwatch->ts->msec_duration= stopwatch->ts->sec_duration*1000;
         stopwatch->ts->usec_duration = stopwatch->ts->sec_duration*1000*1000;
