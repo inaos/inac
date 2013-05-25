249937a51158cda8579362ace1afd9577e46d82b
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-05-25 16:54:58 +0200

    Added assert tests.

--- tests/test_test.c
+++ tests/test_test.c
@@ -58,23 +58,79 @@ INA_TEST_SKIP(test, handle_sigfault) {
     INA_TEST_ASSERT_NOT_NULL(cf->filepath);
 }
 
-INA_TEST(test, assert_equal_str) {
+INA_TEST(test_asserts, assert_equal_str) {
     INA_TEST_ASSERT_EQUAL_STR("test", "test");
 }
 
-INA_TEST(test, assert_not_equal_str) {
+INA_TEST(test_assert, assert_not_equal_str) {
     INA_TEST_ASSERT_NOT_EQUAL_STR("test", "test-1");
 }
 
-INA_TEST(test, assert_data) {
+INA_TEST(test_assert, assert_data) {
     const char* exp = "test";
     const char* real = "test";
     INA_TEST_ASSERT_DATA((const unsigned char*)exp, 4, (const unsigned char*)real, 4);
 }
 
-INA_TEST(test, assert_equal_floating) {
+INA_TEST(test_assert, assert_equal_floating) {
     const double exp = 3.3;
     const double real = 3.3;
     INA_TEST_ASSERT_EQUAL_FLOATING(exp, real);
 }
+
+INA_TEST(test_assert, assert_not_equal_floating) {
+    const double exp = 3.3;
+    const double real = 3.2324;
+    INA_TEST_ASSERT_NOT_EQUAL_FLOATING(exp, real);
+}
+
+INA_TEST(test_assert, assert_equal_integer) {
+    INA_TEST_ASSERT_EQUAL_INTEGER(1, 1);
+}
+
+INA_TEST(test_assert, assert_not_equal_integer) {
+    INA_TEST_ASSERT_NOT_EQUAL_INTEGER(1, 3);
+}
+
+INA_TEST(test_assert, assert_not_null) {
+    int32_t c = 1;
+    INA_TEST_ASSERT_NOT_NULL(&c);
+}
+
+INA_TEST(test_assert, assert_null) {
+    int32_t *c = NULL;
+    INA_TEST_ASSERT_NULL(c);
+}
+
+INA_TEST(test_assert, assert_same) {
+    int32_t i1 = 2;
+    int32_t *i2 = &i1;
+    INA_TEST_ASSERT_SAME(&i1, i2);
+}
+
+INA_TEST(test_assert, assert_not_same) {
+    int32_t i1 = 2;
+    int32_t i2 = 1;
+    int32_t *i3 = NULL;
+    INA_TEST_ASSERT_NOT_SAME(&i1, i3);
+    INA_TEST_ASSERT_NOT_SAME(&i1, &i2);
+    i3 = &i2;
+    INA_TEST_ASSERT_NOT_SAME(&i1, &i3);
+}
+
+INA_TEST(test_assert, assert_true) {
+    INA_TEST_ASSERT_TRUE(1);
+    INA_TEST_ASSERT_TRUE(2);
+    INA_TEST_ASSERT_TRUE(0==0);
+}
+
+INA_TEST(test_assert, assert_false) {
+    INA_TEST_ASSERT_FALSE(0);
+    INA_TEST_ASSERT_FALSE(2!=2);
+}
+
+
+
+
+
 
