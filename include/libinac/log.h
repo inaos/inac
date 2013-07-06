d431f3ee1310fc53203f19dd6ccab77c9851bc2f
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-07-06 18:15:00 +0200

    Fixed log ABI and macro level for win32.

--- include/libinac/log.h
+++ include/libinac/log.h
@@ -45,22 +45,22 @@ extern "C" {
 
 #if INA_LOG_LEVEL>0
 #define INA_LOG_ERROR(cfg,fmt,...)   INA_LOG(cfg, INA_LOG_ERROR, fmt, __VA_ARGS__)
-#elif
+#else
 #define INA_LOG_ERROR(cfg,fmt,...)
 #endif
 #if INA_LOG_LEVEL>1
 #define INA_LOG_WARNING(cfg,fmt,...) INA_LOG(cfg, INA_LOG_WARNING, fmt, __VA_ARGS__)
-#elif
+#else
 #define INA_LOG_WARNING(cfg,fmt,...)
 #endif
 #if INA_LOG_LEVEL>2
 #define INA_LOG_INFO(cfg,fmt,...)    INA_LOG(cfg, INA_LOG_INFO, fmt, __VA_ARGS__)
-#elif
+#else
 #define INA_LOG_INFO(cfg,fmt,...)
 #endif
 #if INA_LOG_LEVEL>3
 #define INA_LOG_DEBUG(cfg,fmt,...)   INA_LOG(cfg, INA_LOG_DEBUG, fmt, __VA_ARGS__)
-#elif
+#else
 #define INA_LOG_DEBUG(cfg,fmt,...)
 #endif
 
@@ -96,7 +96,7 @@ typedef struct ina_log_cfg_s {
 /*
  * Open a log context for based on a log configuration
  */                          
-INA_API(ina_rc_t) ina_log_open(ina_log_cfg_t **cfg, ina_log_target_t target, 
+INA_API(ina_rc_t) ina_log_open(ina_log_cfg_t **cfg, int32_t target, 
                                ina_log_level_t level, const char *logfile);
 
 /*
--- src/log.c
+++ src/log.c
@@ -49,7 +49,7 @@ INA_API(ina_rc_t) ina_log(const ina_log_cfg_t *cfg, ina_log_level_t level, const
     return __ina_log(cfg, level, msg);
 }
 
-INA_API(ina_rc_t) ina_log_open(ina_log_cfg_t **cfg, ina_log_target_t target, 
+INA_API(ina_rc_t) ina_log_open(ina_log_cfg_t **cfg, int32_t target, 
                                 ina_log_level_t level, const char  *logfile)
 {
     *cfg = (ina_log_cfg_t*)ina_mem_alloc(sizeof(ina_log_cfg_t));
