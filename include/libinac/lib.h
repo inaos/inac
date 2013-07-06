08270fc45911f8fa3b67cd0f286f24a32cf9d11e
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-07-06 12:37:16 +0200

    New version 0.2.0

--- include/libinac/lib.h
+++ include/libinac/lib.h
@@ -99,10 +99,10 @@ extern "C" {
  * Version
  */
 #define INA_MAJOR_VERSION 0
-#define INA_MINOR_VERSION 1
+#define INA_MINOR_VERSION 2
 #define INA_MICRO_VERSION 0
 
-#define INA_VERSION       "0.1.0"
+#define INA_VERSION       "0.2.0"
 
 /* Version as a 3-byte hex number, e.g. 0x010201 == 1.2.1. Use this
  * for numeric comparisons, e.g. #if INA_VERSION_HEX >= ... */
