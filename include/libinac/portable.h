4f8af1e7b16216f5b43c2595a874a97922f251d5
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-07-07 10:42:13 +0200

    Added portable path separator constant.

--- include/libinac/portable.h
+++ include/libinac/portable.h
@@ -1055,8 +1055,17 @@ struct timezone {
 INA_API(int) gettimeofday(struct timeval *tv, struct timezone *tz);
 #endif
 
+
+
+#ifdef _WIN32
+#define INA_PATH_SEPARATOR   "\\"
+#else
+#define INA_PATH_SEPARATOR   "/"
+#endif
+
 #ifdef __cplusplus
 }
 #endif 
+
 
 #endif
