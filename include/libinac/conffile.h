34218451003b6bd6d9a414336ac904dee3e3e7df
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-07-07 12:24:53 +0200

    Allow ovveride filepath in INA_CONFFILE() macro.

--- include/libinac/conffile.h
+++ include/libinac/conffile.h
@@ -317,6 +317,7 @@ __VA_ARGS__
     if (!INA_SUCCEED(ina_conffile_process(__cf, fp)))   { \
         abort();                                          \
     }                                                     \
+    if (cf == NULL) cf = __cf;                            \
 }
 
 #ifdef __cplusplus
