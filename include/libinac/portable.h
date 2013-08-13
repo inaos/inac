9b99f66c805376968556bcb7184c61029963d050
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-08-13 19:41:27 +0200

    Make inline works on all platforms and compilers.

--- include/libinac/portable.h
+++ include/libinac/portable.h
@@ -377,14 +377,14 @@ extern "C" {
 #  ifdef INA_COMPILER_MSVC
 #    define INA_INLINE __inline
 #  else
-#   define INA_INLINE inline
+#   define INA_INLINE static inline
 #  endif
 #else
 #  ifdef INA_COMPILER_MSVC
 #    define INA_INLINE __inline
 #  else
 #    ifdef INLINE_ENABLED
-#       define INA_INLINE inline
+#       define INA_INLINE static inline
 #    endif
 #  endif
 #endif
