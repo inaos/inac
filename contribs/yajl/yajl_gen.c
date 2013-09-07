700d311cba4f2a0982681300d299024b5a63f949
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-09-07 05:28:25 +0200

    Fixed string output in case of missing fractional part.

--- contribs/yajl/yajl_gen.c
+++ contribs/yajl/yajl_gen.c
@@ -221,7 +221,10 @@ yajl_gen_double(yajl_gen g, double number)
     ENSURE_VALID_STATE; ENSURE_NOT_KEY; 
     if (isnan(number) || isinf(number)) return yajl_gen_invalid_number;
     INSERT_SEP; INSERT_WHITESPACE;
-    sprintf(i, "%.20g", number);
+    sprintf(i, "%g", number);
+    if (strspn(i, "0123456789-") == strlen(i)) {
+        strcat(i, ".0");
+    }
     g->print(g->ctx, i, (unsigned int)strlen(i));
     APPENDED_ATOM;
     FINAL_NEWLINE;
