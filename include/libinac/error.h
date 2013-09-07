ffa5ac70683e7a1da25394680ff8b117efd9fb1a
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-09-07 05:30:16 +0200

    Added INA_EEMPTY and JSON related errors.

--- include/libinac/error.h
+++ include/libinac/error.h
@@ -96,6 +96,7 @@ extern "C" {
 #define INA_ELOGIC   24
 #define INA_ECAPAC   25
 #define INA_EOVRFL   26
+#define INA_EEMPTY   27
 
 /* Mark an handled error (bit 10 of RC) */
 #define INA_ERR_FLAG_HANDLED 0x200
@@ -271,6 +272,8 @@ extern "C" {
 #define INA_JSON_ERROR(r,s) INA_ERR_PUSH(r, INA_MOD_JSON, INA_OSFN_NONE, s)
 #define INA_JSON_EPOOLF INA_JSON_ERROR(INA_ELOGIC, "Pool is missing entry, make sure to always return");
 #define INA_JSON_EPOOLE INA_JSON_ERROR(INA_ECAPAC, "Pool is exhausted");
+#define INA_JSON_ENOBUF INA_JSON_ERROR(INA_EEMPTY, "Buffer empty");
+                                              
 
 /* Error information */
 typedef struct ina_error_s {
