294ad6f3230adf42d4ae17f71a337b3d270492ab
Rocco Siegrist <rocco.siegrist@matselect.ch>
2013-10-08 15:45:11 +0200

    Enable LUA_USE_APICHECK on debug build (Windows).

--- contribs/luajit/src/msvcbuild_debug.bat
+++ contribs/luajit/src/msvcbuild_debug.bat
@@ -14,7 +14,7 @@
 @if not defined INCLUDE goto :FAIL
 
 @setlocal
-@set LJCOMPILE=cl /nologo /c /MDd /O2 /W3 /D_CRT_SECURE_NO_DEPRECATE
+@set LJCOMPILE=cl /nologo /c /MDd /O2 /W3 /D_CRT_SECURE_NO_DEPRECATE /DLUA_USE_APICHECK
 @set LJLINK=link /nologo
 @set LJMT=mt /nologo
 @set LJLIB=lib /nologo
