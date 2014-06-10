3e76c73d6055684f4a7b92e0940b44eb927bcec8
Rocco Siegrist <rocco.siegrist@matselect.ch>
2014-06-10 15:27:36 +0200

    Fixed succeed macro. Refs #286

--- include/libinac/error.h
+++ include/libinac/error.h
@@ -189,7 +189,7 @@ extern "C" {
 /* Verify if fatal error occurred */
 #define INA_RC_FATAL(rc) ((ina_rc_t)(rc&INA_ERR_FLAG_FATAL))
 /* Check retuen code if successful or handled */
-#define INA_SUCCEED(rc) (INA_SUCCESS == (rc) || INA_RC_REASON((rc)) == 0)
+#define INA_SUCCEED(rc) (INA_SUCCESS == (rc) || INA_RC_HANDLED((rc)))
 
 
 /* Error-Module errors */
