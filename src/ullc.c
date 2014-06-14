9822c430e52c0d2764453af5ad676ab8a4a1e9da
Christian Steiner <christian.steiner@inaos.ch>
2014-06-14 15:26:54 +0200

    windows fix

--- src/ullc.c
+++ src/ullc.c
@@ -370,9 +370,12 @@ INA_API(ina_rc_t) ina_ullc_consumer_get_pos(ina_ullc_ctx_t *ctx, int64_t *pos)
 
 INA_API(ina_rc_t) ina_ullc_consumer_set_pos(ina_ullc_ctx_t *ctx, int64_t pos)
 {
+    int64_t cursor;
+
     INA_ASSERT_NOTNULL(ctx);
     INA_ASSERT_EQUAL(INA_ULLC_CTX_CONSUMER, ctx->type);
-    int64_t cursor = ctx->c_offset->cursor;
+
+    cursor = ctx->c_offset->cursor;
 
     if (pos == -1 || pos > ctx->ring->cursor) {
         pos = ctx->ring->cursor;
