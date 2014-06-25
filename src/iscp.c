fa32f133b472ac43496dff1d769301ef397839ec
Christian Steiner <christian.steiner@inaos.ch>
2014-06-25 18:42:18 +0200

    iscp blocking

--- src/iscp.c
+++ src/iscp.c
@@ -563,10 +563,10 @@ __ina_net_recv_cb(void *user_data, ina_iscp_msg_t *msg)
     if (INA_SUCCEED(ina_net_tcp_accept(&data->fd, data->lfd, NULL, NULL))) {
         if (data->fd != -1) {
             INA_TRACE3("Accepted ISCP fd %d", data->fd);
-            if (!INA_SUCCEED(ina_net_nonblock(data->fd))) {
+            /*if (!INA_SUCCEED(ina_net_nonblock(data->fd))) {
                 ina_net_close(data->fd);
                 data->fd = -1;
-            }
+            }*/
             /*ina_net_set_read_timeout(data->fd, 100);
             ina_net_set_write_timeout(data->fd, 100);*/
         }
