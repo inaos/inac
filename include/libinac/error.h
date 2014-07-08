4a79ec0967cf1d2b2e297bee721f43dd4d32fa29
Rocco Siegrist <rocco@codeundso.ch>
2014-07-08 02:05:09 +0200

    Macro fixed.

--- include/libinac/error.h
+++ include/libinac/error.h
@@ -189,7 +189,7 @@ extern "C" {
 /* Verify if fatal error occurred */
 #define INA_RC_FATAL(rc) ((ina_rc_t)(rc&INA_ERR_FLAG_FATAL))
 /* Check retuen code if successful or handled */
-#define INA_SUCCEED(rc) (INA_SUCCESS == (rc) || INA_RC_HANDLED((rc)))
+#define INA_SUCCEED(rc) (INA_SUCCESS == (rc))
 
 
 /* Error-Module errors */
