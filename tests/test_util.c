ade64b597d81a1170533de6d5fe2e64f0f743ca6
Rocco Siegrist <rocco@codeundso.ch>
2014-09-09 11:35:10 +0200

    Fixed test case.

--- tests/test_util.c
+++ tests/test_util.c
@@ -66,6 +66,7 @@ INA_TEST(util, base64)
     char *ref_decoded = "Base64 is a generic term for a number of similar encoding schemes that encode binary data by treating it numerically and translating it into a base 64 representation. The Base64 term originates from a specific MIME content transfer encoding.";
     char buf[2048];
     size_t outlen;
+    ina_mem_set(&buf, 0, 2048);
 
     INA_TEST_ASSERT_SUCCEED(ina_util_base64_decode_chunk(ref_encoded, strlen(ref_encoded), (unsigned char*)buf, &outlen));
     INA_TEST_ASSERT_EQUAL_STR(ref_decoded, buf);
