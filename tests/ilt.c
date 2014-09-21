54f3c228bbcdd2f892d94d59250c2c480650cc31
Christian Steiner <christian.steiner@inaos.ch>
2014-09-22 00:01:32 +0200

    added ilt binary

--- tests/ilt.c
+++ tests/ilt.c
@@ -29,14 +29,34 @@
 #include <libinac/lib.h>
 
 int main(int argc,  char** argv) 
-{ 
+{
+    ina_rc_t rc;
+    ina_ljit_ctx_t *ctx = NULL;
+    ina_str_t bootstrp;
+
     INA_OPTS(opt,
-        INA_OPT_STRING("b", "bootstrap", "bootstrap.lua", "Full path to the bootstrap file"));
+        INA_OPT_STRING("b", "bootstrap", "<bootstrap>", "Full path to the bootstrap file"));
 
     if (!INA_SUCCEED(ina_app_init(argc, argv, 0, opt))) {
         return EXIT_FAILURE;
     }
 
-    return ina_test_run(argc, argv);
-}
+    if (!INA_SUCCEED(ina_ljit_init(&ctx))) {
+        return EXIT_FAILURE;
+    }
 
+    /* execute the bootstrap if present */
+    ina_opt_get_string("bootstrap", &bootstrp);
+    if (strcmp("<bootstrap>", ina_str_cstr(bootstrp))!=0) {
+        if (luaL_dofile(ctx->lstate, ina_str_cstr(bootstrp)) != 0) {
+            printf("%s", luaL_checkstring(ctx->lstate, 1));
+            return EXIT_FAILURE;
+        }
+    }
+
+    rc = ina_test_run(argc, argv, ctx);
+
+    ina_ljit_destroy(&ctx);
+
+    return rc;
+}
