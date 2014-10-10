03a773cb26a2b9f040692066b3dcbdbfccd28dbd
Christian Steiner <christian.steiner@inaos.ch>
2014-10-10 18:55:39 +0200

    codesonar: 288.4550

--- src/lib.c
+++ src/lib.c
@@ -202,7 +202,7 @@ INA_API(ina_rc_t) ina_app_init(const int argc, char** argv, size_t pool_size, in
                     e = c;
                 }
 
-                if (s > 0) {
+                if (s > 0 && (e-s) >= 0) {
                     char buf[100];
                     strncpy(buf, &argv[n][s], e-s);
                     buf[c-s] = 0;
