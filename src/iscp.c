bf1566068ad03e93dba92c9e7d2303655e198fdb
Rocco Siegrist <rocco@codeundso.ch>
2014-10-12 01:33:26 +0200

    fixed cs warning 407.4946

--- src/iscp.c
+++ src/iscp.c
@@ -774,8 +774,9 @@ __ina_net_recv_cb(void *user_data, ina_iscp_msg_t *msg)
     nb_read = 0;
     
     INA_TRACE3("Acpect ISCP on fd %d", data->lfd);
-    
-    if (INA_SUCCEED(ina_net_tcp_accept(&data->fd, data->lfd, NULL, NULL))) {
+
+    ina_net_tcp_accept(&data->fd, data->lfd, NULL, NULL);    
+    /*if (INA_SUCCEED(ina_net_tcp_accept(&data->fd, data->lfd, NULL, NULL))) {*/
         /*if (data->fd != -1) {*/
             /*INA_TRACE3("Accepted ISCP fd %d", data->fd);*/
             /*if (!INA_SUCCEED(ina_net_nonblock(data->fd))) {
@@ -785,7 +786,7 @@ __ina_net_recv_cb(void *user_data, ina_iscp_msg_t *msg)
             /*ina_net_set_read_timeout(data->fd, 100);
             ina_net_set_write_timeout(data->fd, 100);*/
         /*}*/
-    }
+    /*}*/
     
     if (data->fd == -1) {
         INA_TRACE3("Return EWAIT for next ISCP on fd %d", data->lfd);
