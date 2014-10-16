ae67b7ac28c071bce214361c5efa8baf01f971b8
Rocco Siegrist <rocco@codeundso.ch>
2014-10-16 19:19:01 +0200

    Added asserts for null pointers.

--- src/ljit.c
+++ src/ljit.c
@@ -61,6 +61,8 @@ INA_API(ina_rc_t) ina_ljit_init(ina_ljit_ctx_t **ctx)
     ina_str_t cur_path = NULL;
     ina_str_t new_path = NULL;
 
+    INA_ASSERT_NOTNULL(ctx);
+
     *ctx = (ina_ljit_ctx_t*)ina_mem_alloc(sizeof(ina_ljit_ctx_t));
     if (*ctx == NULL) {
         return INA_ERR_PUSH_LAST;
@@ -88,6 +90,8 @@ INA_API(ina_rc_t) ina_ljit_init(ina_ljit_ctx_t **ctx)
 
 INA_API(ina_rc_t) ina_ljit_destroy(ina_ljit_ctx_t **ctx)
 {
+    INA_ASSERT_NOTNULL(ctx);
+
     if (*ctx == NULL) {
         return INA_SUCCESS;
     }
@@ -99,18 +103,21 @@ INA_API(ina_rc_t) ina_ljit_destroy(ina_ljit_ctx_t **ctx)
 }
 
 unsigned long ina_ljit_hash_sbdm(const char *str)
-{
-   return INA_HASH_CSTR_TO_SDBM(str);
+{ 
+    INA_ASSERT_NOTNULL(str);
+    return INA_HASH_CSTR_TO_SDBM(str);
 }
 
 void ina_ljit_dbl_to_decimal(double dbl, ina_decimal_t *dec)
-{
-   ina_dbl_to_decimal(dbl, dec);
+{   
+    INA_ASSERT_NOTNULL(dec);
+    ina_dbl_to_decimal(dbl, dec);
 }
 
 double ina_ljit_dbl_from_decimal(const ina_decimal_t *dec)
 {
-   return(ina_dbl_from_decimal(dec));
+    INA_ASSERT_NOTNULL(dec);
+    return(ina_dbl_from_decimal(dec));
 }
 
 INA_API(ina_rc_t) ina_ljit_call(ina_ljit_ctx_t *ctx, const char* fname, const char *sig, ...)
@@ -122,13 +129,13 @@ INA_API(ina_rc_t) ina_ljit_call(ina_ljit_ctx_t *ctx, const char* fname, const ch
 
     INA_ASSERT_NOTNULL(ctx);
     INA_ASSERT_NOTNULL(ctx->lstate);
+    INA_ASSERT_NOTNULL(fname);
 
     /* Global function or object method? */
     if (!(cfname = (char*)strchr(fname, '.'))) {
         /* get function */
         lua_getglobal(ctx->lstate, fname); 
-    }
-    else {    
+    } else {    
         ina_str_t obj_name = ina_str_new_fromcstr(fname);
         char *obj_name_c = (char*)ina_str_cstr(obj_name);
         obj_name_c[cfname - fname] = '\0';
@@ -229,6 +236,8 @@ INA_API(ina_rc_t) ina_ljit_dostring(ina_ljit_ctx_t *ctx, const char *code)
 
 INA_API(ina_rc_t) ina_ljit_dump_stack(ina_ljit_ctx_t *ctx)
 {
+    INA_ASSERT_NOTNULL(ctx);
+
     int i = lua_gettop(ctx->lstate);
     fprintf(stdout, " \n----------------  Lua Stack Dump ----------------\n" );
     while(i) {
