4b771b747f9c654972f7d33959c57b0ca8fa12c8
Christian Steiner <christian.steiner@inaos.ch>
2014-11-02 16:32:48 +0100

    fixed headers

--- contribs/timerwheel/timeout.h
+++ contribs/timerwheel/timeout.h
@@ -2,6 +2,7 @@
  * timeout.h - Tickless hierarchical timing wheel.
  * --------------------------------------------------------------------------
  * Copyright (c) 2013, 2014  William Ahern
+ * Copyright (c) 2014, INAOS GmbH
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
