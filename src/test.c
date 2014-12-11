ea298cf7b49c29633b7929d7c970dbf6acb0bba5
Rocco Siegrist <rocco@codeundso.ch>
2014-12-11 01:37:54 +0100

    Fixed buffer overrun.

--- src/test.c
+++ src/test.c
@@ -410,14 +410,14 @@ INA_API(int) ina_test_helper_run(int argc, char *argv[])
     begin = &INA_TEST_TNAME(suite, test);
     end = &INA_TEST_TNAME(suite, test);
  
-    while (1) {
+    while (begin) {
         ina_test_testcase_t* t = begin-1;
         if (t->magic != INA_TEST_MAGIC) {
             break;
         }
         begin--;
     }
-    while (1) {
+    while (end) {
         ina_test_testcase_t* t = end+1;
         if (t->magic != INA_TEST_MAGIC) {
             break;
