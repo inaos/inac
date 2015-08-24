188d7d8e4e5ec9cc43c0f2f3d9e3631ef23d07d5
Rocco Siegrist <rocco@codeundso.ch>
2015-08-24 16:06:47 +0200

    Ensure valid string.

--- tests/test_util.c
+++ tests/test_util.c
@@ -113,16 +113,18 @@ INA_TEST(util, dbl_cmp_save)
 
 INA_TEST(util, sdbm_macro)
 {
-    ina_str_t str;
+    ina_str_t str = NULL;
     str = ina_str_new_fromcstr("test");
+    INA_TEST_ASSERT_NOT_NULL(str);
     INA_TEST_ASSERT_EQUAL_FLOATING(1195757874, INA_HASH_CSTR_TO_SDBM(ina_str_cstr(str)));
     INA_TEST_ASSERT_NOT_EQUAL_FLOATING(3632233, INA_HASH_CSTR_TO_SDBM(ina_str_cstr(str)));
 }
 
 INA_TEST(util, sdbm)
 {
-    ina_str_t str;
+    ina_str_t str = NULL;
     str = ina_str_new_fromcstr("test");
+    INA_TEST_ASSERT_NOT_NULL(str);
     INA_TEST_ASSERT_EQUAL_FLOATING(1195757874, ina_util_hash_sdbm(0, str, ina_str_len(str)));
     INA_TEST_ASSERT_NOT_EQUAL_FLOATING(3632233, ina_util_hash_sdbm(0, str, ina_str_len(str)));
     INA_TEST_ASSERT_EQUAL_FLOATING(1732587620, ina_util_hash_sdbm(1195757874, str, ina_str_len(str)));
@@ -130,16 +132,18 @@ INA_TEST(util, sdbm)
 
 INA_TEST(util, crc32_macro)
 {
-    ina_str_t str;
+    ina_str_t str = NULL;
     str = ina_str_new_fromcstr("test");
+    INA_TEST_ASSERT_NOT_NULL(str);
     INA_TEST_ASSERT_EQUAL_FLOATING(3632233996, INA_HASH_CSTR_TO_CRC32(ina_str_cstr(str)));
     INA_TEST_ASSERT_NOT_EQUAL_FLOATING(3632233, INA_HASH_CSTR_TO_CRC32(ina_str_cstr(str)));
 }
 
 INA_TEST(util, crc32)
 {
-    ina_str_t str;
+    ina_str_t str = NULL;
     str = ina_str_new_fromcstr("test");
+    INA_TEST_ASSERT_NOT_NULL(str);
     INA_TEST_ASSERT_EQUAL_FLOATING(3632233996, ina_util_hash_crc32(0, str, ina_str_len(str)));
     INA_TEST_ASSERT_NOT_EQUAL_FLOATING(3632233, ina_util_hash_crc32(0, str, ina_str_len(str)));
     INA_TEST_ASSERT_EQUAL_FLOATING(3966352177, ina_util_hash_crc32(3632233996, str, ina_str_len(str)));
