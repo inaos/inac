3f63fa5287f1dfd0d08cf1cc01c8a5907895302c
Christian Steiner <christian.steiner@inaos.ch>
2015-10-08 17:47:19 +0200

    added missing function

--- src/net.c
+++ src/net.c
@@ -467,3 +467,7 @@ INA_API(ina_rc_t) ina_net_hw_backend_name(ina_net_hw_ctx_t *ctx, ina_str_t *name
     return INA_SUCCESS;
 }
 
+INA_API(ina_rc_t) ina_net_hw_feature_check(ina_net_hw_ctx_t *ctx, ina_net_hw_feature_t feature)
+{
+    return ctx->funcs.feature_check_fp(ctx, feature);
+}
