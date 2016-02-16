d13745c88535b9be7d1907db381ae87b0ca4f6da
Rocco Siegrist <rocco@codeundso.ch>
2016-02-16 11:06:16 +0100

    implemented faster version to parse /proc

--- src/lprocqry.lua
+++ src/lprocqry.lua
@@ -26,41 +26,70 @@
 -- OF SUCH DAMAGE.
 --
 
+local ffi = require("ffi")
 local jit = require("jit")
-local popen = io.popen
+local iopen = io.open
+
+ffi.cdef[[
+    typedef unsigned long long ino_t;
+    typedef long long off_t;
+    typedef struct __dirstream DIR;
+    struct dirent {
+        ino_t d_ino;             /* inode number */
+        off_t d_off;             /* not an offset; see NOTES */
+        unsigned short d_reclen; /* length of this record */
+        unsigned char d_type;    /* type of file; not supported
+                                    by all filesystem types */
+        char d_name[256];        /* filename */
+    };
+    
+    DIR *opendir(const char *name);
+    struct dirent *readdir(DIR *dirp);
+    int closedir(DIR *dirp);
+]]
 
 local proc_parse = {}
 
 local function _query_process(needle)
-  local f = assert(popen("ls /proc"))
-  for line in f:lines() do
-    if tonumber(line) then
-      local cmd_p = "cat /proc/"..line.."/cmdline"
-      local status_p = "cat /proc/"..line.."/status"
-      local cmd_f = assert(popen(cmd_p))
-      local cmd = cmd_f:read("*a")
-      cmd_f:close()
-      if cmd:find(needle) then
-        local info = {}
-        info._cmd = cmd
-        local status_f = assert(popen(status_p))
-        for sl in status_f:lines() do
-          if sl:find("VmRSS") then
-            local rss = sl:match("%w+%:%s+(%d+)%s%w+")
-            info._rss = tonumber(rss)*1024
+  local dir = ffi.C.opendir("/proc/")
+  if dir == NULL then
+    return nil
+  end
+  local dp = NULL
+  repeat
+    dp = ffi.C.readdir(dir)
+    if dp ~= NULL then
+      local fname = tonumber(ffi.string(dp.d_name))
+      if fname then
+        local cmdline = "/proc/"..fname.."/cmdline"
+        local fcmdline = iopen(cmdline)
+        if fcmdline then
+          local cmd = fcmdline:read("*a")
+          if cmd:find(needle, 1, true) then
+            local info = {}
+            info._cmd = cmd
+            local fstatus = assert(iopen("/proc/"..fname.."/status"))
+            for sl in fstatus:lines() do
+              if sl:find("VmRSS") then
+                local rss = sl:match("%w+%:%s+(%d+)%s%w+")
+                info._rss = tonumber(rss)*1024
+              end
+              if sl:find("Threads") then
+                local threads = sl:match("%w+%:%s+(%d+)")
+                info._threads = threads
+              end
+            end
+            fstatus:close()
+            fcmdline:close()
+            ffi.C.closedir(dir)
+            return info
           end
-          if sl:find("Threads") then
-            local threads = sl:match("%w+%:%s+(%d+)")
-            info._threads = threads
-          end
+          fcmdline:close()
         end
-        status_f:close()
-        f:close()
-        return info
       end
     end
-  end
-  f:close()
+  until dp == NULL
+  ffi.C.closedir(dir)
   return nil
 end
 
