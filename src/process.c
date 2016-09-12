c78b55d660c910764088ed7d8f9fd55dbd151d42
Rocco Siegrist <rocco@codeundso.ch>
2016-09-12 15:27:21 +0200

    Fixed find pointer in a memory pool. Also fixes #402

--- src/process.c
+++ src/process.c
@@ -355,6 +355,7 @@ INA_API(ina_rc_t) ina_process_new(ina_process_ctx_t *ctx,
                                   ina_process_descriptor_t *descriptor, 
                                   ina_process_t **process)
 {
+    ina_mempool_t *mp;
     INA_ASSERT_NOTNULL(descriptor);
     INA_ASSERT_NOTNULL(ctx);
 
@@ -370,10 +371,10 @@ INA_API(ina_rc_t) ina_process_new(ina_process_ctx_t *ctx,
     }
 
     /* copy descriptor if not allocated from context pool */
-    /*if (INA_SUCCEED(ina_mempool_getbypointer(descriptor, &mempool)) && 
-        mempool == ctx->mempool) {
+    if (INA_SUCCEED(ina_mempool_getbypointer(descriptor, &mp)) &&
+        mp == ctx->mempool) {
         (*process)->descriptor = descriptor;
-    } else { */
+    } else {
         (*process)->descriptor = (ina_process_descriptor_t*)ina_mempool_dalloc(
                                         ctx->mempool, 
                                         sizeof(ina_process_descriptor_t));
@@ -401,7 +402,7 @@ INA_API(ina_rc_t) ina_process_new(ina_process_ctx_t *ctx,
         (*process)->descriptor->start_flags = descriptor->start_flags;
         (*process)->descriptor->lifecycle = descriptor->lifecycle;
         (*process)->descriptor->managed_type = descriptor->managed_type;
-    //} 
+    }
     (*process)->descriptor->c_ref = 1;
     (*process)->exit_code = -1;
     (*process)->key = INA_HASH_STR_TO_SDBM(descriptor->full_path);
