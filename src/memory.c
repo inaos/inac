d2d38beecc12116db801731e633c67d1b8e020f0
Rocco Siegrist <rocco@codeundso.ch>
2018-01-31 09:28:47 +0100

    Fixed signed/unsigned assignment

--- src/memory.c
+++ src/memory.c
@@ -495,7 +495,7 @@ INA_API(ina_rc_t) ina_mempool_getinfo(ina_mempool_t *pool, ina_mempool_info_t *i
 
     info->size = 0;
     info->used = 0;
-    info->children = -1;
+    info->children = 0;
     info->cf = pm->cf;
     while (pm != NULL) {
         info->size += pm->size;
