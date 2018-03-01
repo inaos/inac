0d3aa0ae049d8279a6f600024f39673fb93f499e
Rocco Siegrist <rocco@codeundso.ch>
2018-03-01 18:21:01 +0100

    Improved cmake module, #513

--- inac.cmake
+++ inac.cmake
@@ -22,6 +22,36 @@ endif(WIN32)
 
 add_definitions(-DINA_OSTIME_ENABLED -DINA_TIME_DEFINED)
 
+macro(inac_enable_verbose)
+    set(CMAKE_VERBOSE_MAKEFILE ON)
+    message(STATUS "Verbose output enabled")
+endmacro()
+
+function(inac_platform_libs_for_win LIBS)
+    if(WIN32)
+        set(INAC_LIBS_LIST ${PLATFORM_LIBS})
+        list(APPEND INAC_LIBS_LIST "${LIBS}")
+        set(PLATFORM_LIBS ${INAC_LIBS_LIST} PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(inac_platform_libs_for_linux LIBS)
+    if(LINUX)
+        set(INAC_LIBS_LIST ${PLATFORM_LIBS})
+        list(APPEND INAC_LIBS_LIST "${LIBS}")
+        set(PLATFORM_LIBS ${INAC_LIBS_LIST} PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(inac_platform_libs_for_osx LIBS)
+    if(APPLE)
+        set(INAC_LIBS_LIST ${PLATFORM_LIBS})
+        list(APPEND INAC_LIBS_LIST "${LIBS}")
+        set(PLATFORM_LIBS ${INAC_LIBS_LIST} PARENT_SCOPE)
+    endif()
+endfunction()
+
+
 #
 #
 #
@@ -71,16 +101,78 @@ function(inac_add_contrib_lib libname)
     add_library(${libname} ${src})
 endfunction(inac_add_contrib_lib)
 
+macro(inac_add_contrib_lib_win32 libname)
+    if (WIN32)
+        inac_add_contrib_lib(${libname)
+    endif()
+endmacro()
+
+macro(inac_add_contrib_lib_linux libname)
+    if (LINUX)
+        inac_add_contrib_lib(${libname)
+    endif()
+endmacro()
+
+macro(inac_add_contrib_lib_osx libname)
+    if (APPLE)
+        inac_add_contrib_lib(${libname)
+    endif()
+endmacro()
+
 #
 #
 #
-function(inac_add_contrib_lib_ex libname, command)
-    set(INAC_LIBS_LIST ${INAC_LIBS})
-    list(APPEND INAC_LIBS_LIST ${libname})
-    file(GLOB src ${CMAKE_SOURCE_DIR}/contribs/${libname}/*.c)
-    set(INAC_LIBS ${INAC_LIBS_LIST} PARENT_SCOPE)
-    add_library(${libname} ${src})
-endfunction(inac_add_contrib_lib_ex)
+function(inac_add_contrib_lib_ex TARGET DIR PREFIX_YES_NO COMMAND)
+    ExternalProject_Add(${TARGET}
+            PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}
+            CONFIGURE_COMMAND ""
+            URL ${CMAKE_SOURCE_DIR}/contribs/${TARGET}
+            BUILD_COMMAND "${COMMAND}" "${ARGV4}"
+            BUILD_IN_SOURCE 1
+            INSTALL_COMMAND ""
+            )
+    if(WIN32)
+        set(LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/src/${TARGET}/${DIR}")
+        set(prefix "")
+        set(suffix ".lib")
+    else()
+        set(LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}/src/${TARGET}/${DIR}")
+        if("${PREFIX_YES_NO}" STREQUAL "YES")
+            set(prefix "lib")
+        else()
+            set(prefix "")
+        endif()
+        set(suffix ".a")
+    endif()
+
+    set(LIBRARIES "${LIB_DIR}/${prefix}${TARGET}${suffix}")
+    set(INAC_LIBS ${INAC_LIBS} ${LIBRARIES} PARENT_SCOPE)
+    add_dependencies(inac ${TARGET})
+endfunction()
+
+macro(inac_add_contrib_lib_ex_win32 TARGET DIR PREFIX_YES_NO COMMAND)
+    if (WIN32)
+        inac_add_contrib_lib_ex(${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV4}")
+    endif()
+endmacro()
+
+macro(inac_add_contrib_lib_ex_linux TARGET DIR PREFIX_YES_NO COMMAND)
+    if (LINUX)
+        inac_add_contrib_lib_ex(${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} ${ARGV4})
+    endif()
+endmacro()
+
+macro(inac_add_contrib_lib_ex_unix TARGET DIR PREFIX_YES_NO COMMAND)
+    if (NOT WIN32)
+        inac_add_contrib_lib_ex(${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} ${ARGV4})
+    endif()
+endmacro()
+
+macro(inac_add_contrib_lib_ex_osx TARGET DIR PREFIX_YES_NO COMMAND)
+    if (APPLE)
+        inac_add_contrib_lib_ex(${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} ${ARGV4})
+    endif()
+endmacro()
 
 #
 #
@@ -94,7 +186,7 @@ function (inac_add_tests)
     endif()
     file(GLOB src ${CMAKE_SOURCE_DIR}/tests/test_.c helper_*.c)
     add_executable(tests ${CMAKE_CURRENT_BINARY_DIR}/tests.dir/main.c ${src})
-    target_link_libraries(tests inac)
+    target_link_libraries(tests inac ${INAC_LIBS})
 endfunction(inac_add_tests)
 
 #
@@ -109,7 +201,7 @@ function (inac_add_benchmarks)
     endif()
     file(GLOB src ${CMAKE_SOURCE_DIR}/tests/bench/bench_*.c)
     add_executable(bench ${CMAKE_CURRENT_BINARY_DIR}/bench.dir/main.c ${src})
-    target_link_libraries(bench inac)
+    target_link_libraries(bench  inac ${INAC_LIBS})
 endfunction(inac_add_benchmarks)
 
 #
@@ -123,42 +215,17 @@ function(inac_add_tools)
         set(tool ${CMAKE_MATCH_1})
         STRING(REGEX REPLACE "^${CMAKE_SOURCE_DIR}/tools/" "" tool ${tool})
         add_executable(${tool} ${tool_src})
-        target_link_libraries(${tool} ${INAC_LIBS})
+        target_link_libraries(${tool} inac ${INAC_LIBS})
     endforeach()
 endfunction(inac_add_tools)
 
 #
 #
 #
-function(inac_copy_file TARGET FILE)
+function(inac_post_copy_file TARGET FILE)
     add_custom_command(TARGET ${TARGET} POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
             "${PROJECT_SOURCE_DIR}/${FILE}"
             $<TARGET_FILE_DIR:${TARGET}>)
 endfunction()
 
-#
-#
-#
-function(inac_add_luajit)
-    ExternalProject_Add(luajit_local
-            PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/build/luajit
-            CONFIGURE_COMMAND ""
-            URL ${CMAKE_SOURCE_DIR}/contribs/luajit
-            BUILD_COMMAND make
-            BUILD_IN_SOURCE 1
-            INSTALL_COMMAND ""
-            )
-    if(WIN32)
-        set(LUAJIT_LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/luajit/src/luajit_local/src")
-        set(prefix "")
-        set(suffix ".lib")
-    else()
-        set(LUAJIT_LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/luajit/src/luajit_local/src")
-        set(prefix "lib")
-        set(suffix ".a")
-    endif()
-
-    set(LUAJIT_LIBRARIES "${LUAJIT_LIB_DIR}/${prefix}luajit${suffix}")
-    set(INAC_LIBS ${INAC_LIBS} ${LUAJIT_LIBRARIES} PARENT_SCOPE)
-endfunction()
