db34699e98d5abd6a79bd5dea2f57e6a97f73031
Rocco Siegrist <rocco@codeundso.ch>
2018-07-06 09:26:02 +0200

    merge library macro added, #513

--- inac.cmake
+++ inac.cmake
@@ -23,10 +23,10 @@ endif(WIN32)
 
 add_definitions(-DINA_OSTIME_ENABLED -DINA_TIME_DEFINED)
 
-macro(inac_enable_verbose)
+function(inac_enable_verbose)
     set(CMAKE_VERBOSE_MAKEFILE ON)
     message(STATUS "Verbose output enabled")
-endmacro()
+endfunction()
 
 function(inac_platform_libs_for_win LIBS)
     if(WIN32)
@@ -35,6 +35,7 @@ function(inac_platform_libs_for_win LIBS)
         set(PLATFORM_LIBS ${INAC_LIBS_LIST} PARENT_SCOPE)
     endif()
 endfunction()
+
 
 function(inac_platform_libs_for_linux LIBS)
     if(LINUX)
@@ -105,23 +106,23 @@ function(inac_add_contrib_lib libname)
     message(STATUS "Added contrib lib ${libname}")
 endfunction(inac_add_contrib_lib)
 
-macro(inac_add_contrib_lib_win32 libname)
+function(inac_add_contrib_lib_win32 libname)
     if (WIN32)
         inac_add_contrib_lib(${libname})
     endif()
-endmacro()
+endfunction()
 
-macro(inac_add_contrib_lib_linux libname)
+function(inac_add_contrib_lib_linux libname)
     if (LINUX)
         inac_add_contrib_lib(${libname})
     endif()
-endmacro()
+endfunction()
 
-macro(inac_add_contrib_lib_osx libname)
+function(inac_add_contrib_lib_osx libname)
     if (APPLE)
         inac_add_contrib_lib(${libname})
     endif()
-endmacro()
+endfunction()
 
 #
 #
@@ -152,31 +153,32 @@ function(inac_add_contrib_lib_ex DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
     set(LIBRARIES "${LIB_DIR}/${prefix}${TARGET}${suffix}")
     set(INAC_LIBS ${INAC_LIBS} ${LIBRARIES} PARENT_SCOPE)
     add_dependencies(${DEPENDS} ${TARGET})
+    message(STATUS "Added contrib lib ${TARGET}")
 endfunction()
 
-macro(inac_add_contrib_lib_ex_win32 DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+function(inac_add_contrib_lib_ex_win32 DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
     if (WIN32)
         inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
     endif()
-endmacro()
+endfunction()
 
-macro(inac_add_contrib_lib_ex_linux DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+function(inac_add_contrib_lib_ex_linux DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
     if (LINUX)
         inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
     endif()
-endmacro()
+endfunction()
 
-macro(inac_add_contrib_lib_ex_unix DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+function(inac_add_contrib_lib_ex_unix DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
     if (NOT WIN32)
         inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
     endif()
-endmacro()
+endfunction()
 
-macro(inac_add_contrib_lib_ex_osx DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
+function(inac_add_contrib_lib_ex_osx DEPENDS TARGET DIR PREFIX_YES_NO COMMAND)
     if (APPLE)
         inac_add_contrib_lib_ex(${DEPENDS} ${TARGET} ${DIR} ${PREFIX_YES_NO} ${COMMAND} "${ARGV5}")
     endif()
-endmacro()
+endfunction()
 
 #
 #
@@ -292,3 +294,157 @@ function (inac_add_luafiles DIR)
     endforeach()
     set(LUA_OBJECTS ${OBJECTS} PARENT_SCOPE)
 endfunction()
+
+function (inac_amalg_lib LIB LIBS)
+    message(STATUS "Amalg lib ${LIB} with ${LIBS}")
+    ADD_LIBRARY(merged STATIC dummy.c)
+
+    SET_TARGET_PROPERTIES(merged PROPERTIES
+            STATIC_LIBRARY_FLAGS "full\path\to\lib1.lib full\path\to\lib2.lib")
+endfunction()
+
+
+
+# Merge static libraries into a big static lib. The resulting library
+# should not not have dependencies on other static libraries.
+# We use it in MySQL to merge mysys,dbug,vio etc into mysqlclient
+
+MACRO(MERGE_STATIC_LIBS TARGET OUTPUT_NAME LIBS_TO_MERGE)
+# To produce a library we need at least one source file.
+# It is created by ADD_CUSTOM_COMMAND below and will helps
+# also help to track dependencies.
+SET(SOURCE_FILE ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_depends.c)
+ADD_LIBRARY(${TARGET} STATIC ${SOURCE_FILE})
+SET_TARGET_PROPERTIES(${TARGET} PROPERTIES OUTPUT_NAME ${OUTPUT_NAME})
+
+SET(OSLIBS)
+FOREACH(LIB ${LIBS_TO_MERGE})
+GET_TARGET_PROPERTY(LIB_LOCATION ${LIB} LOCATION)
+GET_TARGET_PROPERTY(LIB_TYPE ${LIB} TYPE)
+IF(NOT LIB_LOCATION)
+# 3rd party library like libz.so. Make sure that everything
+# that links to our library links to this one as well.
+LIST(APPEND OSLIBS ${LIB})
+ELSE()
+# This is a target in current project
+# (can be a static or shared lib)
+IF(LIB_TYPE STREQUAL "STATIC_LIBRARY")
+SET(STATIC_LIBS ${STATIC_LIBS} ${LIB_LOCATION})
+ADD_DEPENDENCIES(${TARGET} ${LIB})
+# Extract dependend OS libraries
+GET_DEPENDEND_OS_LIBS(${LIB} LIB_OSLIBS)
+LIST(APPEND OSLIBS ${LIB_OSLIBS})
+ELSE()
+# This is a shared library our static lib depends on.
+LIST(APPEND OSLIBS ${LIB})
+ENDIF()
+ENDIF()
+ENDFOREACH()
+IF(OSLIBS)
+LIST(REMOVE_DUPLICATES OSLIBS)
+TARGET_LINK_LIBRARIES(${TARGET} ${OSLIBS})
+ENDIF()
+
+# Make the generated dummy source file depended on all static input
+# libs. If input lib changes,the source file is touched
+# which causes the desired effect (relink).
+ADD_CUSTOM_COMMAND(
+OUTPUT  ${SOURCE_FILE}
+COMMAND ${CMAKE_COMMAND}  -E touch ${SOURCE_FILE}
+DEPENDS ${STATIC_LIBS})
+
+IF(MSVC)
+# To merge libs, just pass them to lib.exe command line.
+SET(LINKER_EXTRA_FLAGS "")
+FOREACH(LIB ${STATIC_LIBS})
+SET(LINKER_EXTRA_FLAGS "${LINKER_EXTRA_FLAGS} ${LIB}")
+ENDFOREACH()
+SET_TARGET_PROPERTIES(${TARGET} PROPERTIES STATIC_LIBRARY_FLAGS
+"${LINKER_EXTRA_FLAGS}")
+ELSE()
+GET_TARGET_PROPERTY(TARGET_LOCATION ${TARGET} LOCATION)
+IF(APPLE)
+# Use OSX's libtool to merge archives (ihandles universal
+# binaries properly)
+ADD_CUSTOM_COMMAND(TARGET ${TARGET} POST_BUILD
+COMMAND rm ${TARGET_LOCATION}
+COMMAND /usr/bin/libtool -static -o ${TARGET_LOCATION}
+${STATIC_LIBS}
+)
+ELSE()
+# Generic Unix, Cygwin or MinGW. In post-build step, call
+# script, that extracts objects from archives with "ar x"
+# and repacks them with "ar r"
+SET(TARGET ${TARGET})
+CONFIGURE_FILE(
+${MYSQL_CMAKE_SCRIPT_DIR}/merge_archives_unix.cmake.in
+${CMAKE_CURRENT_BINARY_DIR}/merge_archives_${TARGET}.cmake
+@ONLY
+)
+ADD_CUSTOM_COMMAND(TARGET ${TARGET} POST_BUILD
+COMMAND rm ${TARGET_LOCATION}
+COMMAND ${CMAKE_COMMAND} -P
+${CMAKE_CURRENT_BINARY_DIR}/merge_archives_${TARGET}.cmake
+)
+ENDIF()
+ENDIF()
+ENDMACRO()
+
+# Create libs from libs.
+# Merges static libraries, creates shared libraries out of convenience libraries.
+# MERGE_LIBRARIES(target [STATIC|SHARED|MODULE]
+#  [linklib1 .... linklibN]
+#  [EXPORTS exported_func1 .... exportedFuncN]
+#  [OUTPUT_NAME output_name]
+#)
+MACRO(MERGE_LIBRARIES)
+CMAKE_PARSE_ARGUMENTS(ARG
+"EXPORTS;OUTPUT_NAME"
+"STATIC;SHARED;MODULE;NOINSTALL"
+${ARGN}
+)
+LIST(GET ARG_DEFAULT_ARGS 0 TARGET)
+SET(LIBS ${ARG_DEFAULT_ARGS})
+LIST(REMOVE_AT LIBS 0)
+IF(ARG_STATIC)
+IF (NOT ARG_OUTPUT_NAME)
+SET(ARG_OUTPUT_NAME ${TARGET})
+ENDIF()
+MERGE_STATIC_LIBS(${TARGET} ${ARG_OUTPUT_NAME} "${LIBS}")
+ELSEIF(ARG_SHARED OR ARG_MODULE)
+IF(ARG_SHARED)
+SET(LIBTYPE SHARED)
+ELSE()
+SET(LIBTYPE MODULE)
+ENDIF()
+# check for non-PIC libraries
+IF(NOT _SKIP_PIC)
+FOREACH(LIB ${LIBS})
+GET_TARGET_PROPERTY(${LIB} TYPE LIBTYPE)
+IF(LIBTYPE STREQUAL "STATIC_LIBRARY")
+GET_TARGET_PROPERTY(LIB COMPILE_FLAGS LIB_COMPILE_FLAGS)
+STRING(REPLACE "${CMAKE_SHARED_LIBRARY_C_FLAGS}"
+"<PIC_FLAG>" LIB_COMPILE_FLAGS ${LIB_COMPILE_FLAG})
+IF(NOT LIB_COMPILE_FLAGS MATCHES "<PIC_FLAG>")
+MESSAGE(FATAL_ERROR
+"Attempted to link non-PIC static library ${LIB} to shared library ${TARGET}\n"
+"Please use ADD_CONVENIENCE_LIBRARY, instead of ADD_LIBRARY for ${LIB}"
+)
+ENDIF()
+ENDIF()
+ENDFOREACH()
+ENDIF()
+CREATE_EXPORT_FILE(SRC ${TARGET} "${ARG_EXPORTS}")
+ADD_LIBRARY(${TARGET} ${LIBTYPE} ${SRC})
+TARGET_LINK_LIBRARIES(${TARGET} ${LIBS})
+IF(ARG_OUTPUT_NAME)
+SET_TARGET_PROPERTIES(${TARGET} PROPERTIES OUTPUT_NAME "${ARG_OUTPUT_NAME}")
+ENDIF()
+ELSE()
+MESSAGE(FATAL_ERROR "Unknown library type")
+ENDIF()
+IF(NOT ARG_NOINSTALL)
+MYSQL_INSTALL_TARGETS(${TARGET} DESTINATION "${INSTALL_LIBDIR}")
+ENDIF()
+SET_TARGET_PROPERTIES(${TARGET} PROPERTIES LINK_INTERFACE_LIBRARIES "")
+ENDMACRO()
