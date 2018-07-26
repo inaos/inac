aae0e1ce754dbae03a3b53a4790a8d5d96ef4043
Rocco Galli <rocco@codeundso.ch>
2018-07-26 22:06:36 +0200

    Harmonized library namw, #513

--- contribs/cpu-topology/inac_32.bat
+++ contribs/cpu-topology/inac_32.bat
@@ -1,3 +1,3 @@
 REM  Assemble  asm source file for 32-bit environment
 ml /safeseh /c get_cpuid.asm
-lib /NODEFAULTLIB /out:intel-cpu-topo.lib get_cpuid.obj
+lib /NODEFAULTLIB /out:cpu-topology.lib get_cpuid.obj
