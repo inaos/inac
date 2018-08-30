c13437c42b2cf89a7c424811dbb5913750863483
Rocco Galli <rocco@codeundso.ch>
2018-08-30 08:16:31 +0200

    fixed winodows source

--- src/file.c
+++ src/file.c
@@ -343,14 +343,14 @@ INA_API(ina_rc_t) ina_file_stat_synch(ina_file_stat_t *stat,  const ina_file_t *
 	if (!GetFileTime(file->fh, &ct, &at, &wt)) {
 		return INA_OS_ERROR(INA_NN_OPERATION|INA_ERR_FAILED);
 	}
-	(*stat)->file_size = pin.QuadPart;
-	(*stat)->is_dir = (FILE_ATTRIBUTE_DIRECTORY & attrs);
+	stat->file_size = pin.QuadPart;
+	stat->is_dir = (FILE_ATTRIBUTE_DIRECTORY & attrs);
 	FileTimeToSystemTime((FILETIME*)&wt, &systime);
-	__ina_file_system_time_to_time_t(&systime, &(*stat)->mtime);
+	__ina_file_system_time_to_time_t(&systime, &stat->mtime);
 	FileTimeToSystemTime((FILETIME*)&at, &systime);
-	__ina_file_system_time_to_time_t(&systime, &(*stat)->atime);
+	__ina_file_system_time_to_time_t(&systime, &stat->atime);
     _stat(file->file_path, &fst);
-    (*stat)->mode = fst.st_mode;
+    stat->mode = fst.st_mode;
 #else
     struct stat fst;
 
