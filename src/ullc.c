e899c52df2090901030a7b5345500d34327ce8e5
Rocco Galli <rocco@codeundso.ch>
2018-09-25 21:53:00 +0200

    fixed warning

--- src/ullc.c
+++ src/ullc.c
@@ -237,7 +237,7 @@ INA_API(ina_rc_t) ina_ullc_overrun_disable(ina_ullc_ctx_t *ctx)
 
 INA_API(ina_rc_t) ina_ullc_producer_reset(ina_ullc_ctx_t *ctx)
 {
-    INA_UNUSED(ctx;)
+    INA_UNUSED(ctx);
     return INA_SUCCESS;
 }
 
