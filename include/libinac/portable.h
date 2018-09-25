e1f385f83d5c063509be26906219be23f0232d1b
Rocco Galli <rocco@codeundso.ch>
2018-09-25 16:15:48 +0200

    trying to fix format modifiers, #575

--- include/libinac/portable.h
+++ include/libinac/portable.h
@@ -1017,8 +1017,8 @@ struct timezone {
 INA_API(int) gettimeofday(struct timeval *tv, struct timezone *tz);
 #endif
 
-/* int64_t uint64_t format specifiers */
-#define INA_INT64_T_FMT  PRIx64
+#define INA_SIZE_T_FMT  "zu"
+#define INA_INT64_T_FMT  PRINTF_INT64_MODIFIER
 #define INA_UINT64_T_FMT PRINTF_INT64_MODIFIER "u"
 
 /* Pack */
@@ -1046,30 +1046,30 @@ INA_API(int) gettimeofday(struct timeval *tv, struct timezone *tz);
     #error UNSUPPORTED COMPILER
     #endif
 #else
-    #if defined(INA_COMPILER_GCC) || defined(INA_COMPILER_INTEL)
-    #define INA_ALIGNED(x) __attribute__((aligned(x)))
-    #define INA_ALIGNED128 INA_ALIGNED(128)
-    #define INA_ALIGNED64 INA_ALIGNED(64)
-    #define INA_ALIGNED32 INA_ALIGNED(32)
-    #define INA_ALIGNED16 INA_ALIGNED(16)
-    #define INA_ALIGNED8 INA_ALIGNED(8)
-    #define INA_ALIGNED4 INA_ALIGNED(4)
-    #define INA_ALIGNED2 INA_ALIGNED(2)
-    #define INA_VSALIGNED128
-    #define INA_VSALIGNED64
-    #define INA_VSALIGNED32
-    #define INA_VSALIGNED16
-    #define INA_VSALIGNED8
-    #define INA_VSALIGNED4
-    #define INA_VSALIGNED2
-    #ifndef INA_PACKED
-    #define INA_PACKED __attribute__ ((__packed__))
-    #endif
-    #define INA_VS_BEGIN_PACK
-    #define INA_VS_END_PACK
-    #else
-    #error UNSUPPORTED COMPILER
-    #endif
+#    if defined(INA_COMPILER_GCC) || defined(INA_COMPILER_INTEL)
+#    define INA_ALIGNED(x) __attribute__((aligned(x)))
+#    define INA_ALIGNED128 INA_ALIGNED(128)
+#    define INA_ALIGNED64 INA_ALIGNED(64)
+#    define INA_ALIGNED32 INA_ALIGNED(32)
+#    define INA_ALIGNED16 INA_ALIGNED(16)
+#    define INA_ALIGNED8 INA_ALIGNED(8)
+#    define INA_ALIGNED4 INA_ALIGNED(4)
+#    define INA_ALIGNED2 INA_ALIGNED(2)
+#    define INA_VSALIGNED128
+#    define INA_VSALIGNED64
+#    define INA_VSALIGNED32
+#    define INA_VSALIGNED16
+#    define INA_VSALIGNED8
+#    define INA_VSALIGNED4
+#    define INA_VSALIGNED2
+#    ifndef INA_PACKED
+#    define INA_PACKED __attribute__ ((__packed__))
+#    endif
+#    define INA_VS_BEGIN_PACK
+#    define INA_VS_END_PACK
+#    else
+#    error UNSUPPORTED COMPILER
+#    endif
 #endif
 
 #if !defined(GCC_VERSION) || GCC_VERSION <= 30406
@@ -1245,11 +1245,11 @@ void  rewinddir(DIR *dir);
 #   endif
 #endif
 
- #ifdef _DEBUG
- #ifndef DEBUG
- #define DEBUG 1
- #endif
- #endif
+#ifdef _DEBUG
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+#endif
 
 #ifdef __cplusplus
 }
