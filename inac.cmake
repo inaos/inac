c525fc2b69838a93c5cd799fb6770ce2e6a89a01
Rocco Siegrist <rocco@codeundso.ch>
2018-12-11 15:07:42 +0100

    fixed benchmark generation

--- inac.cmake
+++ inac.cmake
@@ -451,7 +451,7 @@ function(inac_add_benchmarks)
     if (NOT EXISTS "${CMAKE_SOURCE_DIR}/bench/main.c")
         if (NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/bench.dir/main.c")
             file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/bench.dir/main.c
-                    "#include <libinac/lib>\nint main(int argc,  char** argv) {  INA_MUST_SUCCEED(ina_app_init(argc, argv, NULL)); return ina_bench_run(argc, argv);}"
+                    "#include <libinac/lib.h>\nint main(int argc,  char** argv) {  INA_MUST_SUCCEED(ina_app_init(argc, argv, NULL)); return ina_bench_run(argc, argv);}"
                     )
         endif ()
         list(APPEND src "${CMAKE_CURRENT_BINARY_DIR}/bench/main.c")
