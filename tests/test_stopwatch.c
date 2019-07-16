a85ee182284e0730ae8da37f68f6b0eb1fb3e8fc
Rocco Siegrist <rocco@codeundso.ch>
2019-07-16 11:53:17 +0200

    more tests #645

--- tests/test_stopwatch.c
+++ tests/test_stopwatch.c
@@ -40,7 +40,8 @@ INA_TEST(stopwatch ,time_stamp)
         ++c;
     }
     INA_TEST_ASSERT_EQUAL_TIME_T(10, c);
-    INA_TEST_ASSERT_SUCCEED(ina_stopwatch_free(&w));
+    ina_stopwatch_free(&w);
+    INA_TEST_ASSERT_NULL(w);
 }
 
 #if !defined (INA_OS_WIN32) && !defined(INA_OS_OSX)
@@ -93,7 +94,7 @@ INA_TEST(stopwatch, stopwatch)
     ina_time_sleep(1);
     INA_TEST_ASSERT_SUCCEED(ina_stopwatch_stop(w));
     INA_TEST_ASSERT_SUCCEED(ina_stopwatch_valid(w));
-    INA_TEST_ASSERT_SUCCEED(ina_stopwatch_free(&w));
+    ina_stopwatch_free(&w);
     INA_TEST_ASSERT_NULL(w);
 }
 
@@ -129,7 +130,7 @@ INA_TEST(stopwatch, stopwatch_startime)
     }
     INA_TEST_ASSERT_SUCCEED(ina_stopwatch_duration(w, &duration));
     INA_TEST_MSG("Duration in secs %.10f", ts->duration);
-    INA_TEST_ASSERT_SUCCEED(ina_stopwatch_free(&w));
+    ina_stopwatch_free(&w);
     INA_TEST_ASSERT_NULL(w);
 }
 
@@ -172,7 +173,7 @@ INA_TEST_SKIP(stopwatch, stopwatch_startime_rdtsc)
     }
     INA_TEST_ASSERT_SUCCEED(ina_stopwatch_duration(w, &duration));
     INA_TEST_MSG("Duration in secs %.10f", duration);
-    INA_TEST_ASSERT_SUCCEED(ina_stopwatch_free(&w));
+    ina_stopwatch_free(&w);
     INA_TEST_ASSERT_NULL(w);
     ina_time_tsc_disable_rdtsc();
 }
@@ -301,3 +302,44 @@ INA_TEST_FIXTURE(stopwatch_ipc_rdtsc, stopwatch_open_rdtsc) {
     INA_TEST_ASSERT_FAILED(ina_stopwatch_started(data->w));
 }
 #endif
+
+static ina_rc_t foreach(void *data) {
+    INA_UNUSED(data);
+    return INA_SUCCESS;
+}
+
+INA_TEST(stopwatch, invalid_arguments)
+{
+    ina_stopwatch_t *w = NULL;
+    ina_time_tsc_t *t = NULL;
+    ina_stopwatch_ts_t *ts = NULL;
+    int fake = 0;
+    double duration = 0;
+    int64_t index = 0;
+
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_new(1, -1, NULL));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_open(1, NULL));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_started(NULL));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_valid(NULL));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_start(NULL, NULL));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_stop(NULL));
+
+    w = (ina_stopwatch_t*)&fake;
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_start_time(NULL, &t));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_start_time(w, NULL));
+
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_stop_time(NULL, &t));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_stop_time(w, NULL));
+
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_duration(NULL, &duration));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_duration(w, NULL));
+
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_read_stamp(NULL, &index, &ts));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_read_stamp(w, NULL, &ts));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_read_stamp(w, &index, NULL));
+
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_foreach_stamp(NULL, foreach));
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_foreach_stamp(w, NULL));
+
+    INA_TEST_ASSERT_ERRMSG(INA_ERR_INVALID_ARGUMENT, ina_stopwatch_stamp(NULL, "u1", "u2"));
+}
