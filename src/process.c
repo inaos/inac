f9ee41d9ab531e37852491dfd78c7376cf5a344d
Rocco Siegrist <rocco@codeundso.ch>
2019-08-04 16:04:30 +0200

    fixed category name, #668

--- src/process.c
+++ src/process.c
@@ -706,7 +706,7 @@ static void __ina_process_stop(ina_process_t *process)
     int still_running = INA_NO;
 
     if (process->pid > 0) {
-        INA_TRACE2(inac,process, "Kill %d", process->pid);
+        INA_TRACE2(inac.process, "Kill %d", process->pid);
         if (kill(process->pid, SIGTERM) == -1) {
             INA_TRACE2(inac.process, "%s", "FAILED to kill");
             process->last_rc = INA_OS_ERROR(INA_ES_PROCESS | INA_ERR_NOT_STOPPED);
